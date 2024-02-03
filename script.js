1f57f3ea93302f58f3bedc3ee286c382f3b5e8c1
Juani-tech <jperezdichiazza@gmail.com>
2024-02-03 19:33:54 -0300

    add basic sketch logic

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const MAX_DIVS = 16;
 const container = document.querySelector('.main-container');
 
 
+
 for (let i = 0; i < MAX_DIVS; i++) {
     const div = document.createElement('div');
     const space = 100/4;
     div.style.flex = `1 1 ${space}%`;
     div.style.backgroundColor = 'black';
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'white';
+    })
+    div.addEventListener('mouseout', () => {
+        div.style.backgroundColor = 'black';
+    })
+
     container.appendChild(div);
 }
